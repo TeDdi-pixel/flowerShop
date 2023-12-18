d4dd082aedf20c3093dfdf727431434feb018266
TeDdi-pixel <2003valuyga2003@gmail.com>
2023-12-18 03:23:47 +0200

    footerTabs optimized

---
+++ src/entities/footerTabs/config/footerData.js
@@ -0,0 +1,27 @@
+import footerLogo from "../../../assets/icons/footer_logo.svg";
+
+export const footerData = [
+  {
+    title: "Menu",
+    links: ["Search", "Home", "About us", "Catalog", "Collections", "Contacts"],
+  },
+  {
+    title: "Address",
+    logo: footerLogo,
+    links: [
+      "6201 Hollywood blvd Los Angeles, California 90028",
+      "Monday - Friday 9:00 am - 8:00 pm Saturday 10:00 am - 5:00 pm Sunday 10:00 am - 5:00 pm",
+      "+214 772 56 74 cherryblossom@gmail.com",
+    ],
+  },
+  {
+    title: "Legal Notice",
+    links: [
+      "Privacy Policy",
+      "Terms of Service",
+      "Refund Policy",
+      "Shipping police",
+      "Billing and payment",
+    ],
+  },
+];
