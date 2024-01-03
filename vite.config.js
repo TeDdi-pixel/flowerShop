d1917f2bd44d73a38dcd88a935721141e27ee170
TeDdi-pixel <2003valuyga2003@gmail.com>
2024-01-04 00:40:47 +0200

    bundle optimization

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import compression from "vite-plugin-compression2";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    compression({
+      algorithm: "gzip",
+      exclude: [/\.(br)$ /, /\.(gz)$/],
+    }),
+    compression({
+      algorithm: "brotliCompress",
+      exclude: [/\.(br)$ /, /\.(gz)$/],
+    }),
+  ],
+});
