06abdafff1379b7d85f5ac06d040093dfa6cac4b
TeDdi-pixel <2003valuyga2003@gmail.com>
2024-01-03 20:22:18 +0200

    useCollections hook optimized

--- src/hooks/useCollections.jsx
+++ src/hooks/useCollections.jsx
@@ -1,63 +1,60 @@
+import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
 import { getDownloadURL, listAll, ref } from "firebase/storage";
 import { useEffect, useState } from "react";
-import { storage, db } from "../services/firebase-config";
-import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
+import { db, storage } from "../services/firebase-config";
 
 const useCollections = (collectionName, folder) => {
   const [urls, setUrls] = useState([]);
   const [collectionsData, setCollectionsData] = useState([]);
 
   const collectionRef = collection(db, collectionName);
-  const getCollectionList = async () => {
-    const cachedData = localStorage.getItem(collectionName);
-    
-    if (cachedData) {
-      setCollectionsData(JSON.parse(cachedData));
-      return;
-    }
-
-    try {
-      const data = await getDocs(collectionRef);
-      const docIds = data.docs.map((doc) => doc.id);
-      for (let i = 0; i < urls.length; i++) {
-        const docRef = doc(collectionRef, docIds[i]);
-
-        updateDoc(docRef, {
-          imageUrl: urls[i],
-        });
-      }
-      const newData = await getDocs(collectionRef);
-      const newFilteredData = newData.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setCollectionsData(newFilteredData);
-
-      localStorage.setItem(collectionName, JSON.stringify(newFilteredData));
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   useEffect(() => {
-    getCollectionList();
+    const getData = async () => {
+      const cachedData = localStorage.getItem(collectionName);
+      if (cachedData) {
+        setCollectionsData(JSON.parse(cachedData));
+      } else {
+        try {
+          const data = await getDocs(collectionRef);
+          const docIds = data.docs.map((doc) => doc.id);
+          for (let i = 0; i < urls.length; i++) {
+            const docRef = doc(collectionRef, docIds[i]);
+            await updateDoc(docRef, {
+              imageUrl: urls[i],
+            });
+          }
+          const newData = await getDocs(collectionRef);
+          const newFilteredData = newData.docs.map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+          }));
+          setCollectionsData(newFilteredData);
+          localStorage.setItem(collectionName, JSON.stringify(newFilteredData));
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+    if (urls.length > 0) {
+      getData();
+    }
   }, [urls]);
 
   useEffect(() => {
     listAll(ref(storage, `${folder}`))
-      .then((res) => {
-        return Promise.all(res.items.map((item) => getDownloadURL(item)));
-      })
+      .then((res) =>
+        Promise.all(res.items.map((item) => getDownloadURL(item)))
+      )
       .then((urls) => {
         setUrls(urls);
       })
       .catch((error) => {
-        console.error(error);
+        console.error(`Error listing all items: ${error}`);
       });
   }, []);
 
   return { collectionsData };
 };
-
 
 export default useCollections;
