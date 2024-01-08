4277b0c2e5d65aae478bda553bbd2342ca786eaa
TeDdi-pixel <2003valuyga2003@gmail.com>
2024-01-08 13:21:56 +0200

    bugfix with config

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-import {getFirestore} from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
@@ -14,4 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import compression from "vite-plugin-compression2";
 import react from "@vitejs/plugin-react";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,30 +17,7 @@ export default defineConfig({
   ],
   optimizeDeps: {
     force: true,
-    include: [
-      'react',
-      'react-dom',
-      'react-hook-form',
-      'react-icons',
-      'react-player',
-      'react-router-dom',
-    ],
+    include: ['react', 'react-dom', 'react-hook-form', 'react-icons', 'react-player', 'react-router-dom'],
     exclude: ['firebase'],
-  },
-  resolve: {
-    dedupe: ['firebase/app', 'firebase/firestore'], // Разрешить дублирование Firebase
-    mainFields: ['module', 'main'],
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        'main.js': './index.html',
-      },
-      output: {
-        manualChunks: {
-          vendor: ['firebase'], // Вынести Firebase в отдельный чанк
-        },
-      },
-    },
   },
 });
