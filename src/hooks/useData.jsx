881694840edcc3ded68b1a88d8a02818d8481d22
TeDdi-pixel <2003valuyga2003@gmail.com>
2024-01-08 13:39:03 +0200

    some more optimization

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -1,31 +1,39 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, onSnapshot, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../services/firebase-config";
 
 const useData = (collectionName) => {
   const [data, setData] = useState([]);
   const collectionRef = collection(db, collectionName);
-  const cachedData = localStorage.getItem(collectionName);
-  useEffect(() => {
-    if (cachedData) {
-      setData(JSON.parse(cachedData));
-    } else {
-      const getData = async () => {
-        try {
-          const querySnapshot = await getDocs(collectionRef);
-          const filteredData = querySnapshot.docs.map((doc) => ({
-            ...doc.data(),
-            id: doc.id,
-          }));
 
-          setData(filteredData);
-          localStorage.setItem(collectionName, JSON.stringify(filteredData));
-        } catch (error) {
-          console.log(error);
-        }
-      };
-      getData();
-    }
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const querySnapshot = await getDocs(collectionRef); // Используем getDocs для начального запроса
+        const filteredData = querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setData(filteredData);
+        localStorage.setItem(collectionName, JSON.stringify(filteredData));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collectionRef, (querySnapshot) => {
+      const filteredData = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setData(filteredData);
+      localStorage.setItem(collectionName, JSON.stringify(filteredData));
+    });
+
+    return () => unsubscribe();
   }, []);
 
   return { data };
